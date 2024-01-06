58d46839e76aa910eb698e9ec3cc0165fadc8c47
GabirelRomo33 <rromo@tiendas-grand.com.mx>
2024-01-06 10:06:29 -0600

    Fin de la Leccion 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
+//import { AppRoutingModule } from './app-routing.module';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from './shared/footer/footer.component';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent]
+    imports: 
+    [
+      CommonModule, 
+      RouterOutlet, 
+      HeaderComponent, 
+      FooterComponent, 
+    ]
 })
 export class AppComponent {
   title = 'portafolio';
